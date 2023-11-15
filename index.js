a83f6dd08665c9854ab99add649b54c419fde852
Spike <spikeelliot+1@gmail.com>
2023-11-15 13:26:36 +0000

    Completed Task 10

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // Import the modules we need
-var express = require ('express')
-var ejs = require('ejs')
-var bodyParser= require ('body-parser')
+var express = require ('express');
+var ejs = require('ejs');
+var bodyParser= require ('body-parser');
+var mysql = require('mysql');
 
 // Create the express application object
-const app = express()
-const port = 8000
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+const port = 8000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define the database connection
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'appuser',
+    password: 'app2027',
+    database: 'myBookshop'
+});
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+
 
 // Set up css
 app.use(express.static(__dirname + '/public'));
@@ -23,10 +41,10 @@ app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
 
 // Define our data
-var shopData = {shopName: "Bertie's Books"}
+var shopData = {shopName: "Bertie's Books"};
 
 // Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
 require("./routes/main")(app, shopData);
 
 // Start the web app listening
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
